e8f210761b0fb3e0cce1c6fd3a2d61e5e023df23
Filip Jinneklint <filip.jinneklint@elev.ga.ntig.se>
2024-12-09 09:07:17 +0100

    sjodhaiodbvuiavdouia

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        if (6>3){
+            System.out.println("Hello, World!");
+        }
+
+        Scanner nameIN = new Scanner(System.in);
+        String name = nameIN.nextLine();
+        Scanner passIN = new Scanner(System.in);
+        String pass = nameIN.nextLine();
+
+        if (name == "noname" && pass == "nopass" ){
+
+        }
+
+        for (int i = 0; i < 32; i++){
+            System.out.println("Hello, World!");
+        }
     }
 }
