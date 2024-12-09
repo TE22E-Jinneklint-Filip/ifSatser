f6126cf3a02e5784b37c24dc251b70e6872a023d
Filip Jinneklint <filip.jinneklint@elev.ga.ntig.se>
2024-12-09 09:56:12 +0100

    sjodhaiodbvuiavdouia

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        if (6>3){
+        if (6 > 3) {
             System.out.println("Hello, World!");
         }
 
-        Scanner nameIN = new Scanner(System.in);
-        String name = nameIN.nextLine();
-        Scanner passIN = new Scanner(System.in);
-        String pass = nameIN.nextLine();
+        while (true) {
+            Scanner nameIN = new Scanner(System.in);
+            System.out.println("Name: ");
+            String name = nameIN.nextLine();
+            Scanner passIN = new Scanner(System.in);
+            System.out.println("Password: ");
+            String pass = nameIN.nextLine();
 
-        if (name == "noname" && pass == "nopass" ){
-
+            if (name.equals("noname") && pass.equals("nopass")) {
+                break;
+            } else {
+            }
         }
 
-        for (int i = 0; i < 32; i++){
+        for (int i = 0; i < 32; i++) {
             System.out.println("Hello, World!");
+        }
+
+        for (int i = 0; i < 5; i++) {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Number: ");
+            int a = in.nextInt();
+            if (a <= 5) {
+                System.out.println("Högre än 5!");
+            } else {
+
+            }
+        }
+
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+
+            try {
+                System.out.println("Number please: ");
+                int num = scan.nextInt();
+                scan.nextLine();
+
+                System.out.println("the number is " + num);
+                break;
+            } catch (Exception error) {
+                scan.nextLine();
+                System.out.println("That is not a numnber");
+            }
+        }
+
+        int slumptal = (int) (Math.random() * 10) + 1;
+        System.out.println("Ett slumpmässigt tal mellan 1 och 10: " + slumptal);
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+
+
+            if ()
         }
     }
 }
